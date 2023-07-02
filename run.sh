e6fb1be530fdc843a92bcf5b3b3003a2c1a4944e
Shaurya <shauryadugar17@gmail.com>
2023-07-02 21:11:23 +0530

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="docker.kashco"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
-  --name=${app} VIRTUAL_HOST=kashconails.in\
+docker run -d -p 56733:80 -e VIRTUAL_HOST=kashconails.in\
+  --name=${app} \
   -v "$PWD:/app" ${app}
